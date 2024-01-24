8280f08340832d93eb9bee949651f74c042200af
pythonwith_dt <emmanueltdortey@yahoo.com>
2024-01-24 20:42:32 +0000

    ex.1.py

---
+++ Variables and Data Types/ex.1.py
@@ -0,0 +1,12 @@
+'''
+Create two variables, name and age, and assign 
+your name and age to them, respectively. 
+Combine them to form a full name and print,
+(Hello (name)! You are (age) years old).
+
+'''
+
+# First solution
+name = "John"
+age = 30
+print("Hello", name, "!You are", age, "years old")
