d9773c0a90d7696c2255386ae5a13dca14c7d1ea
pythonwith_dt <emmanueltdortey@yahoo.com>
2024-01-31 12:26:55 +0000

    Variables and Data Types/Question.4/Q.3.py

--- Variables and Data Types/Question.4/Q.3.py
+++ Variables and Data Types/Question.4/Q.3.py
@@ -2,7 +2,7 @@
 create a new list that contains only the even numbers. 
 Print the original list and the new list. '''
 
-numbers = [12, 34, 56, 78, 90, 89, 43]
+numbers = [12, 21, 34, 43, 49, 56, 64, 78, 88, 90, 99,]
 even_numbers = [num for num in numbers if num % 2 == 0]
 print("Original list:", numbers)
 print("Even numbers:", even_numbers)
