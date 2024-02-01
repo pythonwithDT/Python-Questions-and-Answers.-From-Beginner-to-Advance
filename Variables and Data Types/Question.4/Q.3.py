d857ec2c05577d85c5aea25c7d49b872c90b7485
pythonwith_dt <emmanueltdortey@yahoo.com>
2024-02-01 22:15:02 +0000

    Variables and Data Types/Question.4/explainer.md

--- Variables and Data Types/Question.4/Q.3.py
+++ Variables and Data Types/Question.4/Q.3.py
@@ -6,3 +6,8 @@ numbers = [12, 21, 34, 43, 49, 56, 64, 78, 88, 90, 99,]
 even_numbers = [num for num in numbers if num % 2 == 0]
 print("Original list:", numbers)
 print("Even numbers:", even_numbers)
+
+
+x = "y"
+y = "x"
+print(x is y)
